58edebbdf337c7b6f8cea7b3bddad53a19d3b831
panchenko-kate <kmaslova_007@ukr.net>
2023-03-07 20:38:00 +0200

    added modal window opening

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,17 +8,29 @@ export class App extends Component {
     showModal: false,
   }
 
+  // componentDidMount() {
+
+  // };
+
+  // componentDidUpdate(prevProps, prevState) {
+
+  // };
+
   toggleModal = () => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
     }))
-  }
+  };
 
   render() {
+    const { showModal } = this.state;
+
     return (
       <Application>
-        <Modal />
+        <button type='button' onClick={this.toggleModal}>Open</button>
+        {showModal && 
+        <Modal onClose={this.toggleModal} />}
       </Application>
     )
-  }
+  };
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,36 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 import { Overlay, ModalDiv } from './Modal.styled';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root')
 
 export default class Modal extends Component {
     componentDidMount() {
-        console.log('componentDidMount');
-    }
+        window.addEventListener('keydown', this.handleKeyDown);
+    };
 
     componentWillUnmount() {
-        console.log('componentWillUnmount')
-    }
+        window.removeEventListener('keydown', this.handleKeyDown);
+    };
+
+    handleKeyDown = e => {
+        if(e.code === 'Escape') {
+            this.props.onClose();
+        }
+    };
+
+    handleBackdropClick = e => {
+        if(e.currentTarget === e.target) {
+            this.props.onClose();
+        }
+    };
 
     render() {
-        return (
-            <Overlay>
+        return createPortal(
+            <Overlay onClick={this.handleBackdropClick}>
                 <ModalDiv>{this.props.children} Modal</ModalDiv>
-            </Overlay>
+            </Overlay>,
+            modalRoot,
         );
     }
-}
+};
