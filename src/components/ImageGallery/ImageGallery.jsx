94c268e371795afb07358852ce26d81a27dafea4
panchenko-kate <kmaslova_007@ukr.net>
2023-03-19 00:40:20 +0200

    +

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,11 +3,8 @@ import { Gallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Component } from 'react';
 import Modal from '../Modal/Modal';
-<<<<<<< HEAD
 import Button from "components/Button/Button";
 import { Loader } from "components/Loader/Loader";
-=======
->>>>>>> parent of c4521de (add load mode button)
 
 export default class ImageGallery extends Component {
     state = {
@@ -16,27 +13,19 @@ export default class ImageGallery extends Component {
         loading: false,
         modalImg: null,
         alt: '',
-<<<<<<< HEAD
         page: 1,
         perPage: 12,
         totalItems: null,
-=======
->>>>>>> parent of c4521de (add load mode button)
     };
 
     componentDidUpdate(prevProps, prevState) {
         const nextTag = this.props.tag;
 
-<<<<<<< HEAD
         if (prevProps.tag !== nextTag || prevState.perPage !== this.state.perPage) {
             this.setState({
                 loading: true
              })
             axios.get(`https://pixabay.com/api/?key=33000427-89fe7bf8f999bb2d1ca661cd2&q=${nextTag}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}&page=${this.state.page}`)
-=======
-        if (prevProps.tag !== nextTag) {
-            axios.get(`https://pixabay.com/api/?key=33000427-89fe7bf8f999bb2d1ca661cd2&q=${nextTag}&image_type=photo&orientation=horizontal&per_page=12`)
->>>>>>> parent of c4521de (add load mode button)
             .then(res => res.data.hits)
             .then(pictures => this.setState({ pictures }))
             .catch(error => {
@@ -60,7 +49,6 @@ export default class ImageGallery extends Component {
         });
     };
 
-<<<<<<< HEAD
     loadMoreImages = (pictures) => {
         this.setState(prevState => ({ 
             perPage: prevState.perPage + Number(pictures.perPage)
@@ -70,31 +58,18 @@ export default class ImageGallery extends Component {
 
     render () {
         const { showModal, loading, pictures, tag, modalImg } = this.state;
-=======
-
-    render () {
-        const { showModal, loading, pictures } = this.state;
->>>>>>> parent of c4521de (add load mode button)
         return (
         <div>
         {loading && <Loader />}
         <Gallery className="gallery">
-<<<<<<< HEAD
-=======
-            {loading && <h1>Downloading</h1>}
->>>>>>> parent of c4521de (add load mode button)
             {showModal && 
             <Modal onClose={this.closeModal} 
-            alt={this.state.tag}
-            img={this.state.modalImg}/>}
+            alt={tag}
+            img={modalImg}/>}
             {pictures && 
             <ImageGalleryItem openModal={this.openModal}
             pictures={pictures} />
             }
-<<<<<<< HEAD
-=======
-             
->>>>>>> parent of c4521de (add load mode button)
         </Gallery>
             {pictures !== null && <Button onClick={this.loadMoreImages} />}
         </div>
